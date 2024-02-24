7308cfcb31cf4550fa2124288fa35eb5d4ba3984
sudeshpariyar <sudesh.bullet@gmail.com>
2024-02-24 16:54:20 +1100

    add auth

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,17 @@
 import { type AppType } from "next/app";
+import { ClerkProvider, UserButton } from "@clerk/nextjs";
 
 import { api } from "~/utils/api";
 
 import "~/styles/globals.css";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <ClerkProvider>
+      <UserButton />
+      <Component {...pageProps} />
+    </ClerkProvider>
+  );
 };
 
 export default api.withTRPC(MyApp);
